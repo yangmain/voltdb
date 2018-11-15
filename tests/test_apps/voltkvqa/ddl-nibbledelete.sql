400ef69e5693610249684b7a0b156a7e7e626536
John Wiebe <jwiebe@voltdb.com>
2018-11-15 13:35:45 -0500

    a ddl with an extra timestamps for use with ttl tests

--- tests/test_apps/voltkvqa/ddl-nibbledelete.sql
+++ tests/test_apps/voltkvqa/ddl-nibbledelete.sql
@@ -5,6 +5,36 @@ CREATE TABLE store
   key      varchar(250) not null
 , value    varbinary(1048576) not null
 , inserttime     timestamp DEFAULT NOW NOT NULL
+, f1        varchar(62) default 'a'
+, f2        varchar(62) default 'b'
+, f3        varchar(62) default 'c'
+, f4        varchar(62) default 'a'
+, f5        varchar(62) default 'b'
+, f6        varchar(62) default 'c'
+, f7        varchar(62) default 'a'
+, f8        varchar(62) default 'b'
+, f9        varchar(62) default 'c'
+, f10        varchar(62) default 'a'
+, f11        varchar(62) default 'b'
+, f12        varchar(62) default 'c'
+, f13        varchar(62) default 'a'
+, f14        varchar(62) default 'b'
+, f15        varchar(62) default 'c'
+, f16        varchar(62) default 'a'
+, f17        varchar(62) default 'b'
+, f18        varchar(62) default 'c'
+, f19        varchar(62) default 'a'
+, f20        varchar(62) default 'b'
+, f21        varchar(62) default 'c'
+, f22        varchar(62) default 'a'
+, f23        varchar(62) default 'b'
+, f24        varchar(62) default 'c'
+, f25        varchar(62) default 'a'
+, f26        varchar(62) default 'b'
+, f27        varchar(62) default 'c'
+, f28        varchar(62) default 'a'
+, f29        varchar(62) default 'b'
+, f30        varchar(62) default 'c'
 , PRIMARY KEY (key)
 ,
 );
