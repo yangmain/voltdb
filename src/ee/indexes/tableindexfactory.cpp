f3b1c469dfaa5cf9c84d062a3cbbd82b01735084
Zeeman He <zhe@voltdb.com>
2019-02-28 15:01:54 -0500

    fix compiling

--- src/ee/indexes/tableindexfactory.cpp
+++ src/ee/indexes/tableindexfactory.cpp
@@ -276,19 +276,18 @@ TableIndex *TableIndexFactory::getInstance(const TableIndexScheme &scheme) {
         }
     } else {
         valueCount = scheme.columnIndices.size();
-        int32_t totalColumns = tupleSchema->columnCount() + tupleSchema->hiddenColumnCount();
         for (size_t ii = 0; ii < valueCount; ++ii) {
             int32_t index = scheme.columnIndices[ii];
-            assert(index < totalColumns);
             const TupleSchema::ColumnInfo *columnInfo;
             if (index < tupleSchema->columnCount()) {
                  columnInfo = tupleSchema->getColumnInfo(index);
             } else {
+                 // index on hidden columns
                  index -= tupleSchema->columnCount();
                  columnInfo = tupleSchema->getHiddenColumnInfo(index);
             }
             ValueType exprType = columnInfo->getVoltType();
-            if ( ! isIntegralType(exprType)) {
+            if (!isIntegralType(exprType)) {
                 isIntsOnly = false;
             }
             keyColumnTypes.push_back(exprType);
